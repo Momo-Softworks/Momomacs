4e4f2952cac526bd172b8529d53c26e7181acce2
Samuel Willey <samuel@thewilley.family>
2024-02-12 13:59:46 -0600

    Redirect backups and auto-saves to emacs-user-directory

--- settings.el
+++ settings.el
@@ -4,3 +4,7 @@
 (tool-bar-mode -1) ;; Disable tool bar
 (scroll-bar-mode -1) ;; Disable scroll bar
 (set-face-attribute 'default nil :height 120) ;; Bigger fonts
+
+;; Move backups and auto-saves to emacs folder
+(setq backup-directory-alist `(("." . ,(expand-file-name "backups/" user-emacs-directory))))
+(setq auto-save-file-name-transforms `((".*" ,(expand-file-name "auto-saves/" user-emacs-directory) t)))
