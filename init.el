6d5dee5c1e3f7fa1c7e6acdd7560d0a8457427f0
Samuel Willey <samuel@thewilley.family>
2024-02-12 19:10:28 -0600

    Added README

--- init.el
+++ init.el
@@ -61,15 +61,3 @@
 
 ;; Org
 (momo/load-org-modern)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(jsonrpc)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
